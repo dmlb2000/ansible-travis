334b7c5d9d6df52c9de9987d0911ae77579d398c
David Brown <dmlb2000@gmail.com>
2020-03-06 10:18:26 -0800

    Change the driver to Docker (#24)
    * Change the driver to Docker
    
    This changes the driver to docker and re-creates all the molecule
    test suites to test all the things.
    
    Signed-off-by: David Brown <dmlb2000@gmail.com>
    
    * Try spreading out the load
    
    Signed-off-by: David Brown <dmlb2000@gmail.com>

--- test.sh
+++ test.sh
@@ -5,28 +5,6 @@ PYTHONS=${2:-python3.6 python3.7 python3.8}
 
 for scenario in ${SCENARIOS}; do
   for venv in ${PYTHONS}; do
-    if docker inspect travis > /dev/null 2>&1 ; then
-      docker stop travis
-      sleep 1
-    fi
-    docker run \
-      --rm \
-      -p 2222:22 \
-      --name travis \
-      --privileged -d \
-      -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
-      travisci/ci-ubuntu-1804:packer-1577347966-74db3f91 \
-      /lib/systemd/systemd
-    cat ~/.ssh/id_rsa.pub | \
-      docker exec \
-      -i \
-      -u travis \
-      travis dd of=/home/travis/.ssh/authorized_keys
-    docker exec travis chmod og-rwx /home/travis/.ssh/authorized_keys
-    docker exec travis dpkg-reconfigure openssh-server
-    docker exec travis systemctl restart ssh
     molecule test -s ${scenario}-${venv}
-    docker stop travis
-    sleep 1
   done
 done
